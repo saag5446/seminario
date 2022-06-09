ece0ffe0119010f8dbb77e5aa694579004f2f7fa
unknown <sebastiana.acevedog@ecci.edu.co>
2022-06-08 20:43:29 -0500

    Ajustes

---
+++ programs/programas/menu.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs
+{
+    public class Menu
+    {
+        public static void Inicio()
+        {
+            Program1 p1 = new Program1();
+            p1.fun();
+        }
+    }
+}
