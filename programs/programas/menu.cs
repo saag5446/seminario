e6d24a98750acd473cd5708b54d181da5eb05c74
unknown <lissethy.menesesg@ecci.edu.co>
2022-06-09 19:22:44 -0500

    programa1

--- programs/programas/menu.cs
+++ programs/programas/menu.cs
@@ -10,8 +10,54 @@ namespace programs
     {
         public static void Inicio()
         {
-            Program1 p1 = new Program1();
-            p1.fun();
+            string continuar;
+            int opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Universidad Ecci");
+                Console.WriteLine("Semillero de programacion en .Net y SQL sERVER");
+                Console.WriteLine("Aplicaciones Tipo Consola");
+                Console.WriteLine("1. Operaciones Matematicas ");
+                Console.WriteLine("2. Funciones Matematicas ");
+                Console.WriteLine("3. Validador de Respuestas");
+                Console.WriteLine("4. Salario Semanal de un Operario");
+                Console.WriteLine("5. Estacion Climatica");
+                Console.WriteLine("6. Categorizador de Numeros");
+                Console.WriteLine("7. Estadistica de Pesos");
+                Console.WriteLine("8. Produccion Avicola");
+                Console.WriteLine("9. Sistema de Calificaciones");
+                Console.WriteLine("10. Nomina de Empleados");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                //Implementacion del swich
+                switch (opcion)
+                {
+                    case 1:
+                        Program1 m = new Program1();
+                        m.fun();
+                        break;
+
+                    case 2:
+                        break;
+
+                }
+                Console.WriteLine("Desea repetir el programa de operaciones Matematicas s/n");
+                Console.WriteLine("En caso de seleccionar n el programa vuelve al menu porincipal");
+                continuar = Console.ReadLine();
+            } while (continuar == "S" || continuar == "s");
+
+            /*if(opcion==1)
+            {
+                Programa1 m=new Programa1();
+                m.programas();
+            }
+            if(opcion==2)
+            {
+                Programa2 n=new Programa2();
+                n.programas();
+            }*/
         }
     }
 }
